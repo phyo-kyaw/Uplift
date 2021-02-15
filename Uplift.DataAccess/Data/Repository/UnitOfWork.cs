6590627b6c9d74372b165f07e140a4e0d9fef506
phyo-kyaw <phy0ky6w@gmail.com>
2021-02-15 15:02:18 +1100

    Add project files.

---
+++ Uplift.DataAccess/Data/Repository/UnitOfWork.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Uplift.DataAccess.Data.Repository.IRepository;
+
+namespace Uplift.DataAccess.Data.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+            Category = new CategoryRepository(_db);
+            Frequency = new FrequencyRepository(_db);
+            Service = new ServiceRepository(_db);
+        }
+
+        public ICategoryRepository Category { get; set; }
+        public IFrequencyRepository Frequency { get; set; }
+        public IServiceRepository Service { get; set; }
+
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+
+        public void save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
---
+++ Uplift/Areas/Admin/Controllers/ServiceController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Uplift.DataAccess.Data.Repository.IRepository;
+
+namespace Uplift.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ServiceController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _hostEnviroment;
+
+        public ServiceController(IUnitOfWork unitOfWork, IWebHostEnvironment hostEnvironment)
+        {
+            _unitOfWork = unitOfWork;
+            _hostEnviroment = hostEnvironment;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API call
+
+        public IActionResult GetAll()
+        {
+            return Json(new { data = _unitOfWork.Service.GetAll(includeProperties:"Category,Frequency") });
+        }
+
+
+        #endregion
+
+    }
+}
